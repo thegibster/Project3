29c61542f4b81a95d5c197e28e55f5eea8b79d72
andrew gibbs <andrewgibbs@outlook.com>
2016-05-02 13:50:43 -0700

    the translate works on one side

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -4,10 +4,10 @@
       console.log(socket);
       var user1txt='';
       var user2txt='';
-      var token = "http%3a%2f%2fschemas.xmlsoap.org%2fws%2f2005%2f05%2fidentity%2fclaims%2fnameidentifier=9WdP3&http%3a%2f%2fschemas.microsoft.com%2faccesscontrolservice%2f2010%2f07%2fclaims%2fidentityprovider=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&Audience=http%3a%2f%2fapi.microsofttranslator.com&ExpiresOn=1462218413&Issuer=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&HMACSHA256=S8oAAlLJBBOtJjmHxg5ui9XjHLLbl4XMEPYk%2bsyawFs%3d" ;
+      var token =  "http%3a%2f%2fschemas.xmlsoap.org%2fws%2f2005%2f05%2fidentity%2fclaims%2fnameidentifier=9WdP3&http%3a%2f%2fschemas.microsoft.com%2faccesscontrolservice%2f2010%2f07%2fclaims%2fidentityprovider=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&Audience=http%3a%2f%2fapi.microsofttranslator.com&ExpiresOn=1462222347&Issuer=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&HMACSHA256=cRTyl4%2bsQIUQs6xGwO0pf1gTTVNhxsImGanwdwUwtK0%3d" ;
       $('#btnAjaxTranslate').click(function (evt) {
         var inputText = $('#txtAjaxInput').val();
-        user1txt = inputText;
+        user1txt = $('#txtAjaxInput').val();
         var authToken =token;
         var data = {
           appId: 'Bearer ' + authToken,
@@ -24,12 +24,9 @@
         })
         .done(function (jqXHR, textStatus, errorThrown) {
           console.log('done', this, jqXHR, textStatus, errorThrown);
-          console.log(user1txt);
-           $('#txtAjaxOutput').append(user1txt+"hi");
-           $('#txtAjaxOutput2').append($('#txtAjaxOutput').text(jqXHR));
-            // $('#txtAjaxOutput').text(user1txt+"hi");
-            // $('#txtAjaxOutput2').text($('#txtAjaxOutput').text(jqXHR));
-
+          console.log(jqXHR);
+          $('#txtAjaxOutput').append($('#txtAjaxInput').val()+'<br>');
+          $('#txtAjaxOutput2').append("<div>"+(jqXHR)+"</div>");
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
           console.log('fail', this, jqXHR, textStatus, errorThrown);
@@ -55,8 +52,9 @@
         })
         .done(function (jqXHR, textStatus, errorThrown) {
           console.log('done', this, jqXHR, textStatus, errorThrown);
+          $('#txtAjaxOutput').append($('#txtAjaxOutput2').text(jqXHR));
            $('#txtAjaxOutput2').append(user2txt+"<br>");
-           $('#txtAjaxOutput').append($('#txtAjaxOutput2').text(jqXHR));
+
 
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
