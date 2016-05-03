7c2c576c5b033f75fcbe3002c1be1586831bfb95
martin rosas <martinleonrosas@gmail.com>
2016-05-02 19:06:53 -0700

    OAUTH works

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,21 @@ router.get('/', function(req, res) {
   res.render('index', { user: req.user });
 });
 
+
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
   'google',
   { scope: ['profile', 'email'] }
 ));
 
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout();
