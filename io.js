e7c58e4839610c41534abae0b4f821844675cdf9
Arvin Hadisurya <akhadisurya@gmail.com>
2016-05-04 18:47:39 -0700

    should work now

--- io.js
+++ io.js
@@ -55,37 +55,6 @@ io.on('connection', function (socket) {
 
 
 
-
-
-
-
-
-    // data=bacon;
-    socket.originalLanguage = data.lang;
-    console.log(socket.originalLanguage);
-    rp({
-      method: "GET",
-      uri: "http://api.microsofttranslator.com/V2/Ajax.svc/Translate",
-      qs: {
-        appId: "Bearer" + " " + bacon,
-        from: "en", //chnage to actual values not jquery backside
-        to: "es",
-        text: data.toTrans
-      }
-    })
-    // .then(response => console.log(response))
-    .then(response => io.sockets.emit('back2Front',{response:response,original: data.toTrans,dl:data.lang,dl2:data.lang2,sockLang:socket.originalLanguage}))
-    .catch(err => console.log(err))
-    // io.sockets.emit('back2Front',data);
-  });
-
-
-
-
-
-
-
-
   //Emit the rooms array
   socket.emit('setup', {
     rooms: rooms
